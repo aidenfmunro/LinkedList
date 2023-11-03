6977d41529036e788650d30ae95a0ffafa70de60
aidenfmunro <aidenfmunro@gmail.com>
2023-11-03 21:04:23 +0300

    мега кринж версия :D

--- main.cpp
+++ main.cpp
@@ -1,18 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node 
+struct Node 
 {
     int data;
+    
     Node* next;
 };
+
+struct List
+{
+    Node* head;
+
+    Node* next;
+
+    size_t count;
+};
+
+
+typedef int ErrorCode;
+
+Node* insertNode(int data, int position);
+
+Node* addNode(int data);
+
+int removeNode(int data);
 
 Node* head = NULL;
 
 int main(int argc, char* argv[])
 {
-
+    addNode(1);
+    addNode(2);
+    addNode(3);
+    insertNode(5, 2);
+    Node* curNode = head;
+    for (size_t i = 0; i < 4; i++)
+      {
+        printf("[%d], %d\n", i, curNode->data);
+        curNode = curNode->next;
+      }
     return 0;
+}
+
+List* CreateList(void)
+{
+  List* list = {};
+  list->head = NULL;  
 }
 
 Node* addNode(int data) // O(n) at first sight? -> just add to head :D
@@ -45,6 +79,7 @@ int removeNode(int data)
             else
               {
                 prevNode->next = curNode->next;
+
                 free(curNode);
                 curNode = NULL;
               }
@@ -65,7 +100,12 @@ Node* insertNode(int data, int position)
     while (curNode && position != 0)
       {
         position--;
+        curNode = curNode->next;
       }
     
-    Node* 
+    Node* newNode = (Node*)calloc(1, sizeof(Node));
+
+    newNode->data = data;
+    newNode->next = curNode->next;
+    curNode->next = newNode;
 }
